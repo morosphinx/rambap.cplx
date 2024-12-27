fc1ce908d60a472e478fefcd2bfe60401c8ecbbd
rambap <rambap>
2024-12-27 01:13:23 +0100

    Tests and documentation to define the expected connectivity definition dehavior

---
+++ src/rambap.cplx.UnitTests/Connectivity/ConnectorConfigurations.cs
@@ -0,0 +1,136 @@
+﻿using static rambap.cplx.PartInterfaces.IPartConnectable;
+using static rambap.cplx.UnitTests.Connectivity.ConnectionAction;
+
+namespace rambap.cplx.UnitTests.Connectivity;
+
+internal class Part_InternalCable(bool internalConnected) : Part, IPartConnectable
+{
+    public Connector LeftCableConnector;
+    public Connector RigthCableConnector;
+
+    public void Assembly_Connections(ConnectionBuilder Do)
+    {
+        if(internalConnected)
+            Do.Connect(LeftCableConnector, RigthCableConnector);
+    }
+}
+
+enum ConnectionAction
+{
+    Nothing,
+    Connect,
+    Expose,
+}
+
+internal class Part_ContainerBox(ConnectionAction actionOnL, ConnectionAction actionOnR, bool internalConnected) : Part, IPartConnectable
+{
+    public Connector LeftBoxConnector;
+    public Connector RigthBoxConnector;
+
+    Part_InternalCable Cable = new Part_InternalCable(internalConnected);
+
+    private static void DoConnectionAction(ConnectionBuilder Do, ConnectionAction action, Connector A, Connector B)
+    {
+        switch (action)
+        {
+            case (ConnectionAction.Nothing):
+                break;
+            case (ConnectionAction.Connect):
+                Do.Connect(A, B); // Connect the cable connector to the box connector in a nondescript way
+                break;
+            case (ConnectionAction.Expose):
+                Do.ExposeAs(B, A); // Expose the cable connector on the box exterior
+                break;
+            default:
+                throw new NotImplementedException();
+        };
+    }
+
+    public void Assembly_Connections(ConnectionBuilder Do)
+    {
+        DoConnectionAction(Do, actionOnL, LeftBoxConnector, Cable.LeftCableConnector);
+        DoConnectionAction(Do, actionOnR, RigthBoxConnector, Cable.RigthCableConnector);
+    }
+}
+
+[TestClass]
+public class TestSimpleCableContainer
+{
+    private void TestConnectionCount(
+        ConnectionAction actionOnL, ConnectionAction actionOnR, bool internalConnected,
+        int expectedBoxConnectionCount, bool expectedEndToEndLink)
+    {
+        var B = new Part_ContainerBox(actionOnL, actionOnR, internalConnected);
+        var I = new Pinstance(B);
+        var connectivity = I.Connectivity();
+        Assert.AreEqual(expectedBoxConnectionCount, connectivity!.Connections.Count);
+        // TODO : Assert End To end Link
+        // if internal connected, assert B.LeftBoxConnector and B.RigthBoxConnector are connected
+    }
+
+    //// ---- TESTS WITH CABLE MAKING A CONNECTION
+    // Does it make sens to sometime connect and expose on the same connector ?
+    // If we connect on a connector, it does not make sense to show it as a public part
+    // Differentiate those use cases in the connector definition ?
+
+    // No connection in box, both side
+    [TestMethod] public void TestNNC() => TestConnectionCount(Nothing, Nothing, true, 0, false);
+
+    // One side is connected internaly => No endToEnd connection, one connection to document
+    // Expected Connections (NCC) :
+    // LeftCableConnector   --------(Cable)--------  RightCableConnector
+    // RightCableConnector  -----------------------  RightBoxConnector
+    [TestMethod] public void TestNCC() => TestConnectionCount(Nothing, Connect, true, 2, false); 
+    [TestMethod] public void TestCNC() => TestConnectionCount(Connect, Nothing, true, 2, false);
+
+    // One side is exposed => No endToEnd connection, should document the intenrnal cable
+    // Expected Connections (NEC) :
+    // LeftCableConnector   --------(Cable)--------  RightBoxConnector(RightCableConnector)
+    [TestMethod] public void TestNEC() => TestConnectionCount(Nothing, Expose, true, 1, false);
+    [TestMethod] public void TestENC() => TestConnectionCount(Expose, Nothing, true, 1, false);
+
+
+    // One side is exposed and one connected internaly => Has endToEnd connection, 1 connection to document
+    // Expected Connections (CEC) :
+    // LeftBoxConnector     -----------------------  LeftCableConnector
+    // LeftCableConnector   --------(Cable)--------  RightBoxConnector(RightCableConnector)
+    [TestMethod] public void TestCEC() => TestConnectionCount(Connect, Expose, true, 2, true);
+    [TestMethod] public void TestECC() => TestConnectionCount(Expose, Connect, true, 2, true);
+
+    // Both side connected internaly => Has endToEnd connection, ONE connection to document
+    // Expected Connections (CCC) :
+    // LeftBoxConnector     -----------------------  LeftCableConnector
+    // LeftCableConnector   --------(Cable)--------  RigthCableConnector
+    // RigthCableConnector  -----------------------  RigthBoxConnector
+    [TestMethod] public void TestCCC() => TestConnectionCount(Connect, Connect, true, 3, true);
+
+
+    // Both side exposed => Has endToEnd connection, 1 connection to document
+    // Expected Connections (EEC) :
+    // LeftBoxConnector(LeftCableConnector)   --------(Cable)--------  RightBoxConnector(RightCableConnector)
+    [TestMethod] public void TestEEC() => TestConnectionCount(Expose, Expose, true, 1, true);
+
+
+
+
+
+    //// ---- TESTS WITH CABLE NOT MAKING A CONNECTION
+    // Same number of connections has above, except never has endToEnd connection
+    // TODO : Confirm : While there is no internal signal connection in the cable, it should not change its internal representation in the
+    // Parent connection diagram.
+
+    [TestMethod] public void TestNNN() => TestConnectionCount(Nothing, Nothing, false, 0, false);
+
+    [TestMethod] public void TestNCN() => TestConnectionCount(Nothing, Connect, false, 2, false);
+    [TestMethod] public void TestCNN() => TestConnectionCount(Connect, Nothing, false, 2, false);
+
+    [TestMethod] public void TestNEN() => TestConnectionCount(Nothing, Expose, false, 1, false);
+    [TestMethod] public void TestENN() => TestConnectionCount(Expose, Nothing, false, 1, false);
+
+    [TestMethod] public void TestCEN() => TestConnectionCount(Connect, Expose, false, 2, false);
+    [TestMethod] public void TestECN() => TestConnectionCount(Expose, Connect, false, 2, false);
+
+    [TestMethod] public void TestCCN() => TestConnectionCount(Connect, Connect, false, 3, false);
+
+    [TestMethod] public void TestEEN() => TestConnectionCount(Expose, Expose, false, 1, false);
+}
--- src/rambap.cplx/Modules/Connectivity/PartInterfaces/IPartConnectable.cs
+++ src/rambap.cplx/Modules/Connectivity/PartInterfaces/IPartConnectable.cs
@@ -3,10 +3,23 @@ using rambap.cplx.PartProperties;
 
 namespace rambap.cplx.PartInterfaces;
 
+/// <summary>
+/// Interface implemented by <see cref="Part"/>s that contains electrical wiring and cabling. <br/>
+/// Define first a number of <see cref="Connector"/> on the Part and its component, <br/>
+/// and define connection between those in the <see cref="Assembly_Connections"/> method
+/// </summary>
 public interface IPartConnectable
 {
+    /// <summary>
+    /// Define the connection and exposition of <see cref="Connector"/>s of the Part
+    /// </summary>
+    /// <param name="Do">A <see cref="ConnectionBuilder"/> with the method difining wiirng</param>
     public void Assembly_Connections(ConnectionBuilder Do);
 
+    /// <summary>
+    /// Contains functions to define a part connectivity <br/>
+    /// You cannot create this class directly. Instead, implement <see cref="IPartConnectable"/> on a <see cref="Part"/>
+    /// </summary>
     public class ConnectionBuilder
     {
         public abstract record Cabling
@@ -49,13 +62,26 @@ public interface IPartConnectable
                 throw new InvalidOperationException($"Cabling {cabling} is not owned by this");
         }
 
-        //public void Connect(Connector connector1, Connector connector2)
+        /// <summary>
+        /// Define a connector as an exposition of another. Typical use case : <br/>
+        /// when the connector of a subcomponent is visible on the face of this part. <br/>
+        /// The connectors source and target will refer to the same physical connection point. <br/>
+        /// </summary>
+        /// <param name="source">Connector of a subcomponent. Need to be public</param>
+        /// <param name="target">Connector of this part. Need to be public</param>
         public void ExposeAs(Connector source, Connector target)
         {
             Context.AssertIsParentOf(source);
             Context.AssertIsOwnerOf(target);
             target.DefineAsAnExpositionOf(source);
         }
+
+        /// <summary>
+        /// Same as <see cref="ExposeAs(Connector, Connector)"/>, but instead multiple subcomponent connectors
+        /// are combined into a single connection points
+        /// </summary>
+        /// <param name="sources">Connectors of subcomponents. Need to be public</param>
+        /// <param name="target">Connector of this part. Need to be public</param>
         public void ExposeAs(IEnumerable<Connector> sources, Connector target)
         {
             foreach(var c in sources)
@@ -64,6 +90,14 @@ public interface IPartConnectable
             target.DefineAsAnExpositionOf(sources);
         }
 
+        /// <summary>
+        /// Connect connectorA and connectorB in a non-descript way, generaly wiring. <br/>
+        /// connectorA and connectorB are physicaly distinct connection points. <br/>
+        /// TODO : When generating documentation, left / rigth side of connection diagram will try to match this declaration
+        /// </summary>
+        /// <param name="connectorA">Left side connector. Must be owed by this part or one of its components</param>
+        /// <param name="connectorB">Rigth side connector. Must be owed by this part or one of its components</param>
+        /// <returns>Object representing the created connection</returns>
         public Cabling Connect(Connector connectorA, Connector connectorB)
         {
             Context.AssertIsParentOf(connectorA);
@@ -89,6 +123,9 @@ public interface IPartConnectable
             connector.Signal = signal;
         }
 
+        /// <summary>
+        /// The owning part implementing <see cref="IPartConnectable"/> we are currently processing
+        /// </summary>
         Part Context { get; init; }
         // Internal constructor, prevent usage from outside assembly
         internal ConnectionBuilder(Part context)
