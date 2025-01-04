b6623bf99dff6dd540674a5b119b05c3e35f505f
rambap <->
2025-01-04 12:55:32 +0100

    Added max length of 50 character (~ish) by default to columns listing many CN, to avoid slowdowns and poor usability when having a lot of components on the same line

--- src/rambap.cplx/Modules/Base/Output/IDColumns.cs
+++ src/rambap.cplx/Modules/Base/Output/IDColumns.cs
@@ -10,6 +10,38 @@ namespace rambap.cplx.Modules.Base.Output;
 
 public static class IDColumns
 {
+    private static string JoinWithMaxLength(string delimiter, IEnumerable<string> strings, int maxLength)
+    {
+        List<string> ReturnUntilLengthPassed(IEnumerable<string> strings, int maxLength, out bool allPassed)
+        {
+            allPassed = true;
+            int passedLength = 0;
+            List<string> passedStrings = [];
+            foreach (var s in strings)
+            {
+                if (passedLength > maxLength)
+                {
+                    allPassed = false;
+                    break;
+                }
+                else
+                {
+                    passedLength += s.Length;
+                    passedStrings.Add(s);
+                }
+            }
+            return passedStrings;
+        }
+        bool allDisplayed = false;
+        var displayedStrings = ReturnUntilLengthPassed(strings, maxLength, out allDisplayed);
+        int totalStringCount = strings.Count();
+        int nonDisplayedStringCount = totalStringCount - displayedStrings.Count();
+        // Add a reminder if not all strings are displayed
+        if (!allDisplayed)
+            displayedStrings.Add($"... [{nonDisplayedStringCount} more]");
+        return string.Join(delimiter, displayedStrings);
+    }
+
     public static DelegateColumn<ComponentContent> PartNumber()
         => new DelegateColumn<ComponentContent>("PN", ColumnTypeHint.String,
              i => i.Component.Instance.PN,
@@ -19,12 +51,15 @@ public static class IDColumns
         => new DelegateColumn<ComponentContent>("CN", ColumnTypeHint.String,
             i => i.Component.CN);
 
-    public static DelegateColumn<ComponentContent> GroupCNs()
+    public static DelegateColumn<ComponentContent> GroupCNs(int maxColumnWidth = 50)
        => new DelegateColumn<ComponentContent>("Component CNs", ColumnTypeHint.String,
             i =>
             {
                 var componentCNs = i.AllComponents().Select(c => c.component.CN);
-                return string.Join(", ", componentCNs);
+                if (maxColumnWidth > 0)
+                    return JoinWithMaxLength(", ", componentCNs, maxColumnWidth);
+                else
+                    return string.Join(", ", componentCNs);
             });
 
     public static DelegateColumn<ComponentContent> ComponentID()
@@ -37,7 +72,7 @@ public static class IDColumns
             i => "TOTAL"
             );
 
-    public static DelegateColumn<ComponentContent> GroupCIDs()
+    public static DelegateColumn<ComponentContent> GroupCIDs(int maxColumnWidth = 50)
         => new DelegateColumn<ComponentContent>("Component CIDs", ColumnTypeHint.String,
             i =>
             {
@@ -45,7 +80,10 @@ public static class IDColumns
                     .Select(c => CID.Append(c.location.CIN, c.component.CN))
                     .Select(s => CID.RemoveImplicitRoot(s));
 
-                return string.Join(", ", componentCIDs);
+                if(maxColumnWidth > 0)
+                    return JoinWithMaxLength(", ", componentCIDs, maxColumnWidth);
+                else
+                    return string.Join(", ", componentCIDs);
             });
 
     public static DelegateColumn<ComponentContent> ComponentID_And_Property(string propname) =>
